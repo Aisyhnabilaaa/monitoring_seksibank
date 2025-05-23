7a242a803bb6bbfbde2f892d7a9decd3212b0b14
Amandaaa <manda27122003@gmail.com>
2025-05-23 15:42:57 +0800

    memperbaiki error

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,10 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'http://localhost:3000',
-    },
+      '/api': 'http://localhost:3000'
+    }
   },
+  build: {
+    chunkSizeWarningLimit: 1000 // menambah batas ukuran chunk jadi 1000kB
+  }
 })
